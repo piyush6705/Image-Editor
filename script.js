52f070d6030ee9a59112768ed00f06e1d61e99b7
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-02 03:00:36 +0530

    starting with js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const filters ={
+  brightness: 100,
+  saturation: 100,
+  exposure: 100,
+  saturation: 100,
+  hueRotate: 0,
+  blur: 0,
+  grayScale: 0,
+  sepia: 0,
+  opacity: 100,
+  invert: 0,
+}
+
